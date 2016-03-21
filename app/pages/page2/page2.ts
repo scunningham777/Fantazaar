44ccd68e571508d484f55cbaa6ceab0391da7172
scunningham777 <s.cunningham777@gmail.com>
2016-03-20 22:16:53 -0500

    Initial commit

---
+++ app/pages/page2/page2.ts
@@ -0,0 +1,17 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  template: `
+    <ion-navbar *navbar>
+        <ion-title>
+            All Items
+        </ion-title>
+    </ion-navbar>
+
+    <ion-content class="page2">
+        All Items
+    </ion-content>
+  `
+})
+export class Page2 {}
